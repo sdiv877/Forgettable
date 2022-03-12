5a5725b0fd98a2e660346bb904aa2ae7633754db
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-03-13 10:28:21 +1300

    Update unit tests for new Person model

--- backend/src/routes/__test__/person.route.test.ts
+++ backend/src/routes/__test__/person.route.test.ts
@@ -11,41 +11,62 @@ afterEach(async () => databaseOperations.clearDatabase());
 afterAll(async () => databaseOperations.closeDatabase());
 
 const mockEncounters = [
-  '622ade14db79f3eb6224770e',
-  '622ade396c3ad1d379623505',
-  '622ade3c979482a64ede8e35',
-  '622ade4ae3fb2d1a195f9521'
+  new mongoose.Types.ObjectId('622ade14db79f3eb6224770e'),
+  new mongoose.Types.ObjectId('622ade396c3ad1d379623505'),
+  new mongoose.Types.ObjectId('622ade3c979482a64ede8e35'),
+  new mongoose.Types.ObjectId('622ade4ae3fb2d1a195f9521')
 ]
 
-const requestPersonData = new Array(
+const requestPersonData = new Array<PersonModel>(
   {
-    first_name: 'Sally',
-    last_name: 'Smith',
-    interests: ['a', 'b'],
-    organisation: 'NZ',
-    time_added: new Date('2022-01-01'),
+    full_name: 'Sally Smith',
+    birthday: new Date('2022-01-01'),
+    gender: "",
+    location: "NZ",
+    first_met: new Date('2022-01-01'),
     how_we_met: 'at Uni',
-    birthday: new Date('2002-12-12'),
-    encounters: [mockEncounters[0], mockEncounters[1], mockEncounters[2]]
+    interests: ['a', 'b'],
+    organisation: 'An org',
+    time_updated: new Date('2022-01-01'),
+    social_media: new Map<string, string>([
+      ["github", "gitUser"],
+      ["discord", "discordUser"]
+    ]),
+    image: Buffer.from(""),
+    encounters: [mockEncounters[0], mockEncounters[1], mockEncounters[2]],
   },
   {
-    first_name: 'John',
-    last_name: 'Herriot',
+    full_name: 'John Herriot',
+    birthday: new Date('2022-01-01'),
+    gender: "",
+    location: "NZ",
+    first_met: new Date('2022-01-01'),
+    how_we_met: 'At the library',
     interests: ['a', 'b'],
-    organisation: 'NZ',
-    time_added: new Date('2022-01-01'),
-    how_we_met: 'at the Library',
-    birthday: new Date('2002-12-12'),
+    organisation: 'An org',
+    time_updated: new Date('2022-01-01'),
+    social_media: new Map<string, string>([
+      ["github", "gitUser"],
+      ["discord", "discordUser"]
+    ]),
+    image: Buffer.from(""),
     encounters: [mockEncounters[0], mockEncounters[1]]
   },
   {
-    first_name: 'Kate',
-    last_name: 'James',
+    full_name: 'Kate James',
+    birthday: new Date('2022-01-01'),
+    gender: "",
+    location: "AU",
+    first_met: new Date('2022-01-01'),
+    how_we_met: 'At the Beach',
     interests: ['a', 'b'],
-    organisation: 'AU',
-    time_added: new Date('2022-01-01'),
-    how_we_met: 'at the Beach',
-    birthday: new Date('2002-12-12'),
+    organisation: 'An org',
+    time_updated: new Date('2022-01-01'),
+    social_media: new Map<string, string>([
+      ["github", "gitUser"],
+      ["discord", "discordUser"]
+    ]),
+    image: Buffer.from(""),
     encounters: [mockEncounters[3]]
   })
 
@@ -59,13 +80,12 @@ describe('person', () => {
     const { body } = await supertest(app).get('/api/persons');
     expect(body).toHaveLength(1);
     const result: PersonModel = body[0];
-    expect(result.first_name).toEqual(requestPersonData[0].first_name);
-    expect(result.last_name).toEqual(requestPersonData[0].last_name);
+    expect(result.full_name).toEqual(requestPersonData[0].full_name);
   });
 });
 
 describe('person', () => {
-  it('can be queried by organisation', async () => {
+  it('can be queried by location', async () => {
     for (let i = 0; i < requestPersonData.length; i++) {
       await supertest(app).post('/api/persons')
         .set('Accept', 'application/json')
@@ -73,7 +93,7 @@ describe('person', () => {
         .expect(httpStatus.CREATED);
     }
 
-    const { body } = await supertest(app).get(`/api/persons?organisation=NZ`);
+    const { body } = await supertest(app).get(`/api/persons?location=NZ`);
     expect(body).toHaveLength(2);
   });
 });
